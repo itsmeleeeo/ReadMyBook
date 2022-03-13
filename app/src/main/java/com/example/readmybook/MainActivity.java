dbe79718a8f3c60d25197d4d48863c61b9abe2c3
Leonardo Ferreira <leeeo.ferreira@hotmail.com>
2022-03-13 13:57:57 -0700

    first commit

---
+++ app/src/main/java/com/example/readmybook/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.readmybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView signUp = findViewById(R.id.txtNoAcc);
+        TextView forgotPassword = findViewById(R.id.txtForgotPass);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ForgotPassword.class));
+            }
+        });
+    }
+}
