39bb1230ba98fac72a7961e01ba09ec208443032
alithr21 <malitrix@yahoo.com>
2022-04-03 23:29:05 -0700

    bookEdit,UserProfile,Splasher, Icon, all inputs initiated backend

--- app/src/main/java/com/example/readmybook/Activity_AddABook.java
+++ app/src/main/java/com/example/readmybook/Activity_AddABook.java
@@ -3,12 +3,33 @@ package com.example.readmybook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Activity_AddABook extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
+    EditText txtAddBook= findViewById(R.id.txtBookTitleAddABook);
+    EditText txtBookAuthor= findViewById(R.id.txtAuthorAB);
+    EditText txtPublicationYear = findViewById(R.id.txtPublicationYearAB);
+    EditText txtPublisher = findViewById(R.id.txtPublisherAB);
+    EditText txtIsbn = findViewById(R.id.txtIsbnAB);
+    Button btnAddBook = findViewById(R.id.btnAddBookAB);
+    Spinner spinRentOutAmount = findViewById(R.id.spinRentOutAmountAB);
+    Spinner spinGenre = findViewById(R.id.spinGenreAB);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_abook);
+
+        btnAddBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
